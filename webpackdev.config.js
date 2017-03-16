9ea3b7cdc5b718441ea4489f739c66a5a64da6a0
ty20130216 <ty20130216@163.com>
2017-03-17 02:12:18 +0800

    try to use redux

--- webpackdev.config.js
+++ webpackdev.config.js
@@ -1,7 +1,43 @@
 var webpack = require('webpack');
-var path = require('path'); 
-var HtmlwebpackPlugin = require('html-webpack-plugin');
-var config = require("./webpack.config.js");
-config.entry.unshift('webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:3333');
-module.exports = config;
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3333',//
+        'webpack/hot/only-dev-server',//
+        './app/index.js'
+    ],
+    module: {
+        rules: [{
+            test: /date-time\.js$/,
+            loaders: ['muiLocal', 'babel-loader']
+        }, {
+            test: /\.jsx?$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/
+        }, {
+            test: /\.(css|less)$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.less$/,
+            loader: "less-loader"
+        }, {
+            test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            loader: 'file-loader'
+        }, {
+            test: /\.(jpg|png)$/,
+            loader: 'file?name=[path][name].[hash].[ext]'
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+        libraryTarget: 'umd'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()//generate hot update chunks
+    ]
+};
