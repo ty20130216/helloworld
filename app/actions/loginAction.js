236fd26c915efdc1f687fa549de935e784450fa5
Tiger Zheng <ty20130216@163.com>
2017-04-24 12:21:07 +0800

    0425
    weather

--- app/actions/loginAction.js
+++ app/actions/loginAction.js
@@ -16,3 +16,31 @@ export const setData = (path, data) => {
     }
 }
 
+export const loadWeather = (city) => {
+    return dispatch => {
+        reqwest({
+            method:'get',
+            url: 'http://api.openweathermap.org/data/2.5/weather?APPID=b714ec74bbab5650795063cb0fdf5fbe',
+            data:{
+                q : "[" + city +"]"
+            }
+            , type: 'json',
+            crossOrigin: true
+
+        })
+            .then(function (resp) {
+                dispatch( {
+                    type: actionType.setData,
+                    data: resp.weather[0].description,
+                    path: ['weather']
+                })
+            }, function (err, msg) {
+                dispatch( {
+                    type: actionType.setData,
+                    data: '',
+                    path: ['weather']
+                })
+                console.log("error")
+            })
+    }
+}
--- app/components/summary.js
+++ app/components/summary.js
@@ -6,7 +6,33 @@ export default class Summary extends React.Component{
     constructor(props) {
         super(props);
     }
+	loadweather(city){
+        let self = this
+        if(city.length === 0){
+            return
+        }
+        reqwest({
+            method:'get',
+            url: 'http://api.openweathermap.org/data/2.5/weather?APPID=b714ec74bbab5650795063cb0fdf5fbe',
+            data:{
+                q : "[" + city +"]"
+            }
+            , type: 'json',
+            crossOrigin: true
 
+        })
+            .then(function (resp) {
+                self.props.actions.setData(['weather'],resp.weather[0].description)
+            }, function (err, msg) {
+                self.props.actions.setData(['weather'],'')
+                console.log("error")
+            })
+
+    }
+
+    componentWillMount(){
+        this.loadweather(this.props.city)
+    }
    
     render() {
         return (
